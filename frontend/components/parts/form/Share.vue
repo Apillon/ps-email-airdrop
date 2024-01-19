2a79a46bbc279c9ecbc2275d605b988fa584dc38
Urban Kovač <urban.kovac@kalmia.si>
2024-01-19 13:19:20 +0100

    Claim (added metadata - image)

--- frontend/components/parts/form/Share.vue
+++ frontend/components/parts/form/Share.vue
@@ -1,19 +1,23 @@
-<script lang="ts" setup>
-import NftPNG from '~/assets/images/nft.png';
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <div class="max-w-md w-full md:px-6 my-12 mx-auto">
     <div class="my-8 text-center">
       <h3 class="mb-6">Celebrate your triumph!</h3>
-      <p>Display Your 'NFTino' NFT Collectible on Social Media for All to Envy.</p>
+      <p>Display Your 'NFT Basic' NFT Collectible on Social Media for All to Envy.</p>
     </div>
 
     <div class="rounded-lg overflow-hidden mb-8">
-      <img :src="NftPNG" class="" width="400" height="400" alt="nft" />
+      <img
+        src="https://bafybeiek6y5unwzvseyvc627ykdtnov7k6g27rjfelwfmafri45ciluxhe.ipfs.nectarnode.io/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaWQiOiJiYWZ5YmVpZWs2eTV1bnd6dnNleXZjNjI3eWtkdG5vdjdrNmcyN3JqZmVsd2ZtYWZyaTQ1Y2lsdXhoZSIsInByb2plY3RfdXVpZCI6IjMwNmIxNGRlLWIwZTEtNGMwYi05OGVkLWEwZWQwZWEzODg2OSIsImlhdCI6MTcwNTY2NjY0Mywic3ViIjoiSVBGUy10b2tlbiJ9.Gli699CsKJR4FU-JRISXaxocZ92UfAw2Rdrjv3VvKtU"
+        class=""
+        width="400"
+        height="400"
+        alt="nft"
+      />
 
       <div class="p-6 bg-bg-lighter">
-        <h5>NFT Nino</h5>
+        <h5>NFT Basic</h5>
       </div>
     </div>
 
